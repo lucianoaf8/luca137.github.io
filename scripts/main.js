40cb3c01ca9d868ae039684142eb4e7674b5c23b
Luciano A <lucianoaf8@gmail.com>
2024-08-11 12:56:33 -0600

    Change simple design

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,6 @@ import { initializeHeader } from './components/header.js';
 import { initializeHero } from './components/hero.js';
 import { initializeSkills } from './components/skills.js';
 import { initializePortfolio } from './components/portfolio.js';
-import { initializeParticles } from './components/particles.js';
 import { initializeTextGenerator } from './components/textGenerator.js';
 
 async function initializeApp() {
