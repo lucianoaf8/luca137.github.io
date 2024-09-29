16cc46a660ab5d7cbb4c09b763ecbc45163708d4
Luciano A <lucianoaf8@gmail.com>
2024-09-29 16:27:05 -0600

    Add deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,21 @@
+// svelte.config.js
 import adapter from '@sveltejs/adapter-static';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
   kit: {
     adapter: adapter({
-      fallback: '200.html',  // or another fallback page
+      fallback: '404.html',  // Enables SPA mode with a fallback page
+      // The following options use default values suitable for GitHub Pages
+      pages: 'build',
+      assets: 'build',
+      precompress: false,
+      strict: true
     }),
-  },
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH  // Ensures base path is root
+    }
+  }
 };
+
+export default config;
